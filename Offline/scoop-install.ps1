aedebba998afbaa5e4abbf0658f3ae1ae90e09e6
sparke <sdhfullmetal@hotmail.com>
2023-01-30 18:55:57 +0800

    modified:   Offline/scoop-install.ps1

--- Offline/scoop-install.ps1
+++ Offline/scoop-install.ps1
@@ -1,38 +1,34 @@
 # Turn on logging, default location C:\Users\sdh\Documents, here we use Path to specify to the current script location
 $DATE = (Get-Date -Format 'yyyy-MM-dd')
-$LOG_FILE = "${PWD}\logs\${DATE}-Scoop_install.log"
+$LOG_FILE = "${PWD}\logs\${DATE}_Scoop-install.log"
 Start-Transcript -Path $LOG_FILE -Append -Force -NoClobber | Out-Null
 
 # Use the scoop command to check if scoop is installed, if it does not exist then it will be installed automatically
 function Install-Scoop {
-  $EMPTY = "[ \f\n\r\t\v]"
-  $NCP = "[a-zA-Z]:\\"
-  $CURRENT_USER = (Read-Host -Prompt 'Please Enter The Current-User Installation Path ("C:\Scoop"e.g.)')
-  $GLOBAL_USER = (Read-Host -Prompt 'Please Enter The Global-User Installation Path ("C:\ScoopGlobal"e.g.)')
-  $DEFAULT_CURRENTUSER = "${env:USERPROFILE}\Scoop"
-  $DEFAULT_GLOBALUSER = "${env:ProgramData}\ScoopGlobal"
-
   try {
     Write-Host "scoop Installed" -ForegroundColor Green -BackgroundColor Black | scoop --version
   }
   catch {
-    $SCOOP_DIR = ${CURRENT_USER}
-    $SCOOP_GLOBAL_DIR = ${GLOBAL_USER}
-    if (${SCOOP_DIR}, ${SCOOP_GLOBALDIR} -imatch ${NCP}) { 
-      Write-Host "The Path Of Your Installation Is ${SCOOP_DIR}, ${SCOOP_GLOBALDIR}" -ForegroundColor Green -BackgroundColor Black
+    $CURRENT_USER = Read-Host -Prompt 'Please Enter The Current-User Installation Path (e.g. "C:\Scoop")'
+    $GLOBAL_USER = Read-Host -Prompt 'Please Enter The Global-User Installation Path (e.g. "C:\ScoopGlobal")'
+    $DEFAULT_CURRENT_USER = "${env:USERPROFILE}\Scoop"
+    $DEFAULT_GLOBAL_USER = "${env:ProgramData}\ScoopGlobal"
+
+    if (${CURRENT_USER}, ${GLOBAL_USER} -imatch "^[a-zA-Z]:\\") { 
+      Write-Host "The Path Of Your Installation Is ${CURRENT_USER}, ${GLOBAL_USER}" -ForegroundColor Green -BackgroundColor Black
     } 
-    elseif (${SCOOP_DIR}, ${SCOOP_GLOBALDIR} -imatch ${EMPTY}) {
-      $SCOOP_DIR = ${DEFAULT_CURRENTUSER}
-      $SCOOP_GLOBALDIR = ${DEFAULT_GLOBALUSER}
-      Write-Host "The Path Of Your Installation Is ${DEFAULT_CURRENTUSER}, ${DEFAULT_GLOBALUSER}" -ForegroundColor Blue -BackgroundColor Black
+    elseif (${CURRENT_USER}, ${GLOBAL_USER} -imatch "^[ \f\n\r\t\v]+$") {
+      ${CURRENT_USER} = ${DEFAULT_CURRENT_USER}
+      ${GLOBAL_USER} = ${DEFAULT_GLOBAL_USER}
+      Write-Host "The Path Of Your Installation Is ${DEFAULT_CURRENT_USER}, ${DEFAULT_GLOBAL_USER}" -ForegroundColor Blue -BackgroundColor Black
     }
     else {
       Clear-Host
-      Write-Host "Wrong Format, Please Follow The Prompts To Enter The Correct Format Path, If You Want To Install To The 'Default Location' You Can Enter 'Space'" -ForegroundColor Red -BackgroundColor Black
+      Write-Host "Wrong Format, Please Follow The Prompts To Enter The Correct Format Path, If You Want To Install To The 'Default Location' You Can Enter Spaces" -ForegroundColor Red -BackgroundColor Black
       powershell -Command . "${PSCommandPath}"
       return
     }
-    . "${PSScriptRoot}\Installation\install.ps1" -ScoopDir ${SCOOP_DIR} -ScoopGlobalDir ${SCOOP_GLOBALDIR} -NoProxy | Out-Null
+    . "${PSScriptRoot}\Installation\install.ps1" -ScoopDir ${CURRENT_USER} -ScoopGlobalDir ${GLOBAL_USER} -NoProxy
   }
 }
 
@@ -66,27 +62,13 @@ Install-App
 
 # Add available bucket
 function Install-Bucket {
-  $MAIN = (Get-Content "${PWD}\src\main.txt")
-  $JAVA = (Get-Content "${PWD}\src\java.txt")
-  $EXTRAS = (Get-Content "${PWD}\src\extras.txt")
-  $DORADO = (Get-Content "${PWD}\src\dorado.txt")
-  $SCOOPCN = (Get-Content "${PWD}\src\scoopcn.txt")
-  $VERSIONS = (Get-Content "${PWD}\src\versions.txt")
-  # Delete bucket
-  $MAIN | ForEach-Object { scoop bucket rm main "${PSItem}" }
-  $JAVA | ForEach-Object { scoop bucket rm java "${PSItem}" }
-  $EXTRAS | ForEach-Object { scoop bucket rm extras "${PSItem}" }
-  $DORADO | ForEach-Object { scoop bucket rm dorado "${PSItem}" }
-  $SCOOPCN | ForEach-Object { scoop bucket rm scoopcn "${PSItem}" }
-  $VERSIONS | ForEach-Object { scoop bucket rm versions "${PSItem}" }
-  # Add bucket
-  $MAIN | ForEach-Object { scoop bucket add main "${PSItem}" }
-  $JAVA | ForEach-Object { scoop bucket add java "${PSItem}" }
-  $EXTRAS | ForEach-Object { scoop bucket add extras "${PSItem}" }
-  $DORADO | ForEach-Object { scoop bucket add dorado "${PSItem}" }
-  $SCOOPCN | ForEach-Object { scoop bucket add scoopcn "${PSItem}" }
-  $VERSIONS | ForEach-Object { scoop bucket add versions "${PSItem}" }
-  # Example：$MAIN | ForEach-Object -Begin {Write-Output "add main"} -Process {scoop bucket add main "$_"} -End {Write-Output "ok"}
+  $BUCKET_FILES = @("main", "java", "extras", "dorado", "scoopcn", "versions")
+  
+  foreach (${BUCKET} in ${BUCKET_FILES}) {
+    ${BUCKET_CONTENT} = Get-Content "${PWD}\src\${BUCKET}.txt"
+    ${BUCKET_CONTENT} | ForEach-Object { scoop bucket rm ${BUCKET} "${PSItem}" }
+    ${BUCKET_CONTENT} | ForEach-Object { scoop bucket add ${BUCKET} "${PSItem}" }
+  }
 }
 
 Install-Bucket
