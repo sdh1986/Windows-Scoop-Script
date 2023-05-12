a86864048d6b18e36145e41fc98b97c718625d6a
sparke <sdhfullmetal@hotmail.com>
2023-05-12 15:19:41 +0800

    modified:   Online/scoop-install.ps1

--- Online/scoop-install.ps1
+++ Online/scoop-install.ps1
@@ -1,7 +1,7 @@
 # Turn on logging, default location C:\Users\sdh\Documents, here we use Path to specify to the current script location.
 $DATE = (Get-Date -Format 'yyyy-MM-dd')
-$LOG_FILE = "${PWD}\logs\${DATE}_Scoop-install.log"
-Start-Transcript -Path $LOG_FILE -Append -Force -NoClobber | Out-Null
+$LOG_FILE = "$PWD\logs\$DATE_Scoop-install.log"
+Start-Transcript -Path ${LOG_FILE} -Append -Force -NoClobber | Out-Null
 
 # Use the scoop command to check if scoop is installed, if it does not exist then it will be installed automatically.('Invoke-Expression' = '&' = '.')
 function Install-Scoop {
@@ -11,8 +11,8 @@ function Install-Scoop {
     catch {
         $CURRENT_USER = Read-Host -Prompt 'Please Enter The Current-User Installation Path (e.g. C:\Scoop)'
         $GLOBAL_USER = Read-Host -Prompt 'Please Enter The Global-User Installation Path (e.g. C:\ScoopGlobal)'
-        $DEFAULT_CURRENT_USER = "${env:USERPROFILE}\Scoop"
-        $DEFAULT_GLOBAL_USER = "${env:ProgramData}\ScoopGlobal"
+        $DEFAULT_CURRENT_USER = "$env:USERPROFILE\Scoop"
+        $DEFAULT_GLOBAL_USER = "$env:ProgramData\ScoopGlobal"
 
         if (${CURRENT_USER}, ${GLOBAL_USER} -imatch '^[a-zA-Z]:\\') { 
             Write-Host "The Path Of Your Installation Is ${CURRENT_USER}, ${GLOBAL_USER}" -ForegroundColor Green -BackgroundColor Black
@@ -25,10 +25,10 @@ function Install-Scoop {
         else {
             Clear-Host
             Write-Host "Wrong Format, Please Follow The Prompts To Enter The Correct Format Path, If You Want To Install To The 'Default_Installation' You Can Enter 'Spacebar'." -ForegroundColor Red -BackgroundColor Black
-            powershell -Command . ${PSCommandPath}
+            powershell -Command . $PSCommandPath
             return
         }
-        . "${PSScriptRoot}\Installation\install.ps1" -ScoopDir ${CURRENT_USER} -ScoopGlobalDir ${GLOBAL_USER} -NoProxy
+        . "$PSScriptRoot\Installation\install.ps1" -ScoopDir ${CURRENT_USER} -ScoopGlobalDir ${GLOBAL_USER} -NoProxy
     }
 }
 
@@ -42,10 +42,10 @@ function Install-App {
   
     # Installation
     try {
-        Write-Host '7-Zip Installed.' -ForegroundColor Green -BackgroundColor Black | 7z | Where-Object { ${PSItem} -like "*7-Zip*" }
-        Write-Host 'git   Installed.' -ForegroundColor Green -BackgroundColor Black | git --version | Where-Object { ${PSItem} -like "*git*" }
-        Write-Host 'gsudo Installed.' -ForegroundColor Green -BackgroundColor Black | gsudo --version | Where-Object { ${PSItem} -like "*gsudo*" }
-        Write-Host 'aria2 Installed.' -ForegroundColor Green -BackgroundColor Black | aria2c --version | Where-Object { ${PSItem} -like "*aria2 version*" }
+        Write-Host '7-Zip Installed.' -ForegroundColor Green -BackgroundColor Black | 7z | Where-Object { $PSItem -like "*7-Zip*" }
+        Write-Host 'git   Installed.' -ForegroundColor Green -BackgroundColor Black | git --version | Where-Object { $PSItem -like "*git*" }
+        Write-Host 'gsudo Installed.' -ForegroundColor Green -BackgroundColor Black | gsudo --version | Where-Object { $PSItem -like "*gsudo*" }
+        Write-Host 'aria2 Installed.' -ForegroundColor Green -BackgroundColor Black | aria2c --version | Where-Object { $PSItem -like "*aria2 version*" }
     }
     catch {
         scoop install ${7ZIP}
@@ -62,12 +62,16 @@ Install-App
 
 # Add available bucket.
 function Install-Bucket {
-    $BUCKET_FILES = @("php", "main", "java", "extras", "dorado", "nirsoft", "scoopcn", "versions", "nonportable", "sysinternals", "scoop-cn", "nerd-fonts", "nirsoft-alternative")
+    $BUCKET_FILES = @("sparke", "php", "main", 
+        "java", "extras", "dorado", "nirsoft", 
+        "scoopcn", "versions", "nonportable", "sysinternals",
+        "scoop-cn", "nerd-fonts", "nirsoft-alternative"
+    )
     
-    foreach (${BUCKET} in ${BUCKET_FILES}) {
-        $BUCKET_CONTENT = Get-Content "${PWD}\src\${BUCKET}.txt"
-        ${BUCKET_CONTENT} | ForEach-Object { scoop bucket rm ${BUCKET} ${PSItem} }
-        ${BUCKET_CONTENT} | ForEach-Object { scoop bucket add ${BUCKET} ${PSItem} }
+    foreach ($BUCKET in ${BUCKET_FILES}) {
+        $BUCKET_CONTENT = Get-Content "$PWD\src\${BUCKET}.txt"
+        ${BUCKET_CONTENT} | ForEach-Object { scoop bucket rm ${BUCKET} $PSItem }
+        ${BUCKET_CONTENT} | ForEach-Object { scoop bucket add ${BUCKET} $PSItem }
     }
 }
 
@@ -76,7 +80,7 @@ Install-Bucket
 # Installing custom software.('Invoke-Expression' = '&' = '.')
 function Install-Software {
     try {
-        $INSTALL_SOFTWARE = "${PSScriptRoot}\scoopbackup\backups\backup"
+        $INSTALL_SOFTWARE = "$PSScriptRoot\scoopbackup\backups\backup"
         Invoke-Expression ${INSTALL_SOFTWARE}-*.ps1
         Invoke-Expression ${INSTALL_SOFTWARE}-*.bat
         Write-Host 'Software Installation Completed Successfully.' -ForegroundColor Green -BackgroundColor Black
@@ -91,7 +95,7 @@ Install-Software
 # Backup all software installed by scoop.('Invoke-Expression' = '&' = '.')
 function Backup-Scoop {
     try {
-        $BACKUP_SCOOP = "${PSScriptRoot}\scoopbackup\scoop-backup.ps1"
+        $BACKUP_SCOOP = "$PSScriptRoot\scoopbackup\scoop-backup.ps1"
         & ${BACKUP_SCOOP}
         & ${BACKUP_SCOOP} --compress
         Write-Host 'Backup Scoop Completed Successfully.' -ForegroundColor Green -BackgroundColor Black
