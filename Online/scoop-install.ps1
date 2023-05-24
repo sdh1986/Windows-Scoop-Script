967da7d0c6e2e9a01009772a188539a0ac54c08d
sparke <sdhfullmetal@hotmail.com>
2023-05-24 15:34:12 +0800

    renamed:    Online/app/appinstallation_currentuser.txt -> Online/Installation/app/appinstallation_currentuser.txt 	renamed:    Online/src/dorado.txt -> Online/Installation/src/dorado.txt 	renamed:    Online/src/extras.txt -> Online/Installation/src/extras.txt 	renamed:    Online/src/java.txt -> Online/Installation/src/java.txt 	renamed:    Online/src/main.txt -> Online/Installation/src/main.txt 	renamed:    Online/src/nerd-fonts.txt -> Online/Installation/src/nerd-fonts.txt 	renamed:    Online/src/nirsoft-alternative.txt -> Online/Installation/src/nirsoft-alternative.txt 	renamed:    Online/src/nirsoft.txt -> Online/Installation/src/nirsoft.txt 	renamed:    Online/src/nonportable.txt -> Online/Installation/src/nonportable.txt 	renamed:    Online/src/php.txt -> Online/Installation/src/php.txt 	renamed:    Online/src/scoop-cn.txt -> Online/Installation/src/scoop-cn.txt 	renamed:    Online/src/scoopcn.txt -> Online/Installation/src/scoopcn.txt 	renamed:    Online/src/sysinternals.txt -> Online/Installation/src/sysinternals.txt 	renamed:    Online/src/versions.txt -> Online/Installation/src/versions.txt 	renamed:    Online/src/zapps.txt -> Online/Installation/src/zapps.txt 	modified:   Online/scoop-install.ps1

--- Online/scoop-install.ps1
+++ Online/scoop-install.ps1
@@ -3,33 +3,32 @@ $DATE = (Get-Date -Format 'yyyy-MM-dd')
 $LOG_FILE = "$PSScriptRoot\installation\logs\${DATE}_Scoop-Install.log"
 Start-Transcript -Path ${LOG_FILE} -Append -Force -NoClobber | Out-Null
 
-# Use the scoop command to check if scoop is installed, if it does not exist then it will be installed automatically.('Invoke-Expression' = '&' = '.')
 function Install-Scoop {
-  try {
-      Write-Host "scoop Installed." -ForegroundColor Green | scoop --version
-  }
-  catch {
-      $CURRENT_USER = Read-Host -Prompt 'Please Enter The Current-User Installation Path (e.g. C:\Scoop)'
-      $GLOBAL_USER = Read-Host -Prompt 'Please Enter The Global-User Installation Path (e.g. C:\ScoopGlobal)'
-      $DEFAULT_CURRENT_USER = "$env:USERPROFILE\Scoop"
-      $DEFAULT_GLOBAL_USER = "$env:ProgramData\ScoopGlobal"
+    try {
+        Write-Host "scoop Installed." -ForegroundColor Green | scoop --version
+    }
+    catch {
+        $CURRENT_USER = Read-Host -Prompt 'Please Enter The Current-User Installation Path (e.g. C:\Scoop)'
+        $GLOBAL_USER = Read-Host -Prompt 'Please Enter The Global-User Installation Path (e.g. C:\ScoopGlobal)'
+        $DEFAULT_CURRENT_USER = "$env:USERPROFILE\Scoop"
+        $DEFAULT_GLOBAL_USER = "$env:ProgramData\ScoopGlobal"
 
-      if (${CURRENT_USER}, ${GLOBAL_USER} -imatch '^[a-zA-Z]:\\') { 
-          Write-Host "The Path Of Your Installation Is ${CURRENT_USER}, ${GLOBAL_USER}" -ForegroundColor Green
-      } 
-      elseif (${CURRENT_USER}, ${GLOBAL_USER} -imatch '^[ \f\n\r\t\v]+$') {
-          ${CURRENT_USER} = ${DEFAULT_CURRENT_USER}
-          ${GLOBAL_USER} = ${DEFAULT_GLOBAL_USER}
-          Write-Host "The Path Of Your Installation Is ${DEFAULT_CURRENT_USER}, ${DEFAULT_GLOBAL_USER}" -ForegroundColor Blue
-      }
-      else {
-          Clear-Host
-          Write-Host "Wrong Format, Please Follow The Prompts To Enter The Correct Format Path, If You Want To Install To The 'Default_Installation' You Can Enter 'Spacebar'." -ForegroundColor Red
-          powershell -Command . $PSCommandPath
-          return
-      }
-      . "$PSScriptRoot\Installation\install.ps1" -ScoopDir ${CURRENT_USER} -ScoopGlobalDir ${GLOBAL_USER} -NoProxy
-  }
+        if (($CURRENT_USER -match '^([a-zA-Z]:\\[^/:*?"<>|]*)$') -and ($GLOBAL_USER -match '^([a-zA-Z]:\\[^/:*?"<>|]*)$')) {
+            Write-Host "The Path Of Your Installation Is ${CURRENT_USER}, ${GLOBAL_USER}" -ForegroundColor Green
+        }
+        elseif (($CURRENT_USER -match '^[ \f\n\r\t\v]+$') -or ($GLOBAL_USER -match '^[ \f\n\r\t\v]+$')) {
+            $CURRENT_USER = $DEFAULT_CURRENT_USER
+            $GLOBAL_USER = $DEFAULT_GLOBAL_USER
+            Write-Host "The Path Of Your Installation Is ${DEFAULT_CURRENT_USER}, ${DEFAULT_GLOBAL_USER}" -ForegroundColor Blue
+        }
+        else {
+            Clear-Host
+            Write-Host "Wrong Format, Please Follow The Prompts To Enter The Correct Format Path, If You Want To Install To The 'Default_Installation' You Can Enter 'Spacebar'." -ForegroundColor Red
+            powershell -Command . $PSCommandPath
+            return
+        }
+        . "$PSScriptRoot\Installation\install.ps1" -ScoopDir ${CURRENT_USER} -ScoopGlobalDir ${GLOBAL_USER} -NoProxy
+    }
 }
 
 Install-Scoop
