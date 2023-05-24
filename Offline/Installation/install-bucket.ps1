da934eb472ae4d81a4dcecfc9a6834a2171bf515
sparke <sdhfullmetal@hotmail.com>
2023-05-24 12:10:07 +0800

    modified:   Offline/Installation/install-bucket.ps1

--- Offline/Installation/install-bucket.ps1
+++ Offline/Installation/install-bucket.ps1
@@ -13,7 +13,7 @@ function Install-Bucket {
     $BUCKET_CONTENT | ForEach-Object { scoop bucket rm $BUCKET $PSItem }
     $BUCKET_CONTENT | ForEach-Object { scoop bucket add $BUCKET $PSItem }
   }
-#    irm https://gitee.com/sdhsparke/online-installer/raw/master/%20Online-FastGithubUninstaller.ps1 | iex
+    & $env:TEMP\Fastgithub_Installer\fastgithub-uninstaller.cmd
     gsudo Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
     gsudo Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock' -Name 'AllowDevelopmentWithoutDevLicense' -Value 1
     git config --global http.sslverify true
