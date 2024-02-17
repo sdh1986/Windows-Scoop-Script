17b9565bb121d1b40a658506fde743b942dd66aa
sdh1986 <sdhfullmetal@hotmail.com>
2024-02-17 21:57:10 +0800

    modified:   bin/auto-pr.ps1 	modified:   bin/checkhashes.ps1 	modified:   bin/checkurls.ps1 	modified:   bin/checkver.ps1 	new file:   bin/describe.ps1 	modified:   bin/formatjson.ps1 	new file:   bin/install.ps1 	modified:   bin/missing-checkver.ps1 	new file:   bin/refresh.ps1 	new file:   bin/scoop.ps1 	modified:   bin/test.ps1 	new file:   bin/uninstall.ps1

--- bin/missing-checkver.ps1
+++ bin/missing-checkver.ps1
@@ -1,6 +1,52 @@
-#Requires -Version 5.1
+<#
+.SYNOPSIS
+    Check if manifest contains checkver and autoupdate property.
+.PARAMETER App
+    Manifest name.
+    Wirldcard is supported.
+.PARAMETER Dir
+    Location of manifests.
+.PARAMETER SkipSupported
+    Manifests with checkver and autoupdate will not be presented.
+#>
+param(
+    [String] $App = '*',
+    [Parameter(Mandatory = $true)]
+    [ValidateScript( {
+        if (!(Test-Path $_ -Type Container)) {
+            throw "$_ is not a directory!"
+        } else {
+            $true
+        }
+    })]
+    [String] $Dir,
+    [Switch] $SkipSupported
+)
 
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$missing_checkver = "$env:SCOOP_HOME\bin\missing-checkver.ps1"
-$dir = "$psscriptroot\..\bucket" # checks the parent dir
-Invoke-Expression -Command "$missing_checkver -dir $dir $($args | ForEach-Object { "$_ " })"
+. "$PSScriptRoot\..\lib\core.ps1"
+. "$PSScriptRoot\..\lib\manifest.ps1"
+
+$Dir = Convert-Path $Dir
+
+Write-Host '[' -NoNewLine
+Write-Host 'C' -NoNewLine -ForegroundColor Green
+Write-Host ']heckver'
+Write-Host ' | [' -NoNewLine
+Write-Host 'A' -NoNewLine -ForegroundColor Cyan
+Write-Host ']utoupdate'
+Write-Host ' |  |'
+
+Get-ChildItem $Dir -Filter "$App.json" -Recurse | ForEach-Object {
+    $json = parse_json $_.FullName
+
+    if ($SkipSupported -and $json.checkver -and $json.autoupdate) { return }
+
+    Write-Host '[' -NoNewLine
+    Write-Host $(if ($json.checkver) { 'C' } else { ' ' }) -NoNewLine -ForegroundColor Green
+    Write-Host ']' -NoNewLine
+
+    Write-Host '[' -NoNewLine
+    Write-Host $(if ($json.autoupdate) { 'A' } else { ' ' }) -NoNewLine -ForegroundColor Cyan
+    Write-Host '] ' -NoNewLine
+    Write-Host $_.BaseName
+}
--- bin/test.ps1
+++ bin/test.ps1
@@ -1,6 +1,1 @@
-#Requires -Version 5.1
-#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '4.4.0' }
-
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$result = Invoke-Pester "$PSScriptRoot\..\test" -PassThru
-exit $result.FailedCount
+. "$PSScriptRoot\..\test\bin\test.ps1"
