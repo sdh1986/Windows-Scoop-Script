f6bc7f463e4e2fb0421c497b2d8197c69647511c
sparke <31507699+sdh1986@users.noreply.github.com>
2026-08-30 04:08:55 +0800

    Refactor software names in install-software.ps1
    Removed commented-out software names and adjusted the array of software names for installation.

--- installation/install-software.ps1
+++ installation/install-software.ps1
@@ -1,11 +1,12 @@
 # Install Software
 function Install-Software {
-  #$SOFTWARE_NAMES = @("scoop-cn/aria2", "scoop-cn/dark", "scoop-cn/innounp", 
-  #"scoop-cn/scoop-search")
   $SOFTWARE_NAMES = @("scoop-cn/aria2", "scoop-cn/dark", "scoop-cn/innounp", 
-  "scoop-bucket/cms6", "sparkebucket/univpn-np", "sparkebucket/xylink-np", 
-  "sparkebucket/sinopharmwps365-np", "sparkebucket/sdriveclient-np", 
-  "scoop-cn/wecom", "scoop-cn/wechat")
+  "scoop-cn/scoop-search", "sparkebucket/univpn-np", 
+  "sparkebucket/sinopharmwps365-np")
+  #$SOFTWARE_NAMES = @("scoop-cn/aria2", "scoop-cn/dark", "scoop-cn/innounp", 
+  #"scoop-bucket/cms6", "sparkebucket/univpn-np", "sparkebucket/xylink-np", 
+  #"sparkebucket/sinopharmwps365-np", "sparkebucket/sdriveclient-np", 
+  #"scoop-cn/wecom", "scoop-cn/wechat")
 
   $installed = @()
   $failed = @()
