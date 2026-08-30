0e25f4ce6f00aab65cfc8a632a21db12f483f17b
sparke <31507699+sdh1986@users.noreply.github.com>
2026-08-30 18:27:43 +0800

    Add WeChat to software installation list

--- installation/install-software.ps1
+++ installation/install-software.ps1
@@ -2,7 +2,7 @@
 function Install-Software {
   $SOFTWARE_NAMES = @("scoop-cn/aria2", "scoop-cn/dark", "scoop-cn/innounp", 
   "scoop-cn/scoop-search", "sparkebucket/univpn-np", 
-  "sparkebucket/sinopharmwps365-np")
+  "sparkebucket/sinopharmwps365-np", "scoop-cn/wechat")
   #$SOFTWARE_NAMES = @("scoop-cn/aria2", "scoop-cn/dark", "scoop-cn/innounp", 
   #"scoop-bucket/cms6", "sparkebucket/univpn-np", "sparkebucket/xylink-np", 
   #"sparkebucket/sinopharmwps365-np", "sparkebucket/sdriveclient-np", 
